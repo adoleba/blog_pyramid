b4ef97faa2edeaa38e8cbfd885633d8c4eeba80a
adoleba <ania.doleba@wp.pl>
2020-10-07 14:47:19 +0200

    category model

--- blog_pyramid/models/post.py
+++ blog_pyramid/models/post.py
@@ -1,9 +1,11 @@
 import datetime
 
+from slugify import slugify
 from sqlalchemy.orm import relationship
 
 from blog_pyramid.models.meta import Base
-from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Index
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
+from blog_pyramid.models.category import posts_categories
 
 
 class Post(Base):
@@ -14,7 +16,10 @@ class Post(Base):
     body = Column(String)
     created = Column(DateTime, default=datetime.datetime.utcnow)
     edited = Column(DateTime, default=datetime.datetime.utcnow)
-
+    slug = Column(String(40), unique=True)
     author = relationship('User', back_populates='posts')
     author_username = Column(String, ForeignKey('user.username'))
+    categories = relationship('Category', secondary=posts_categories, back_populates='posts')
 
+    def __init__(self, title):
+        self.slug = slugify(title)
