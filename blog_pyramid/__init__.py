21d355b454caa1a194352e3131cd79c2bb82d4c6
adoleba <ania.doleba@wp.pl>
2020-10-07 13:34:01 +0200

    init custom admin panel

--- blog_pyramid/__init__.py
+++ blog_pyramid/__init__.py
@@ -8,6 +8,5 @@ def main(global_config, **settings):
         config.include('.models')
         config.include('pyramid_jinja2')
         config.include('.routes')
-        config.include('pyramid_sacrud', route_prefix='admin')
         config.scan()
     return config.make_wsgi_app()
