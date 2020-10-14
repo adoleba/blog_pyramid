b3b98f1d866d9f53bde44c41e350c55adffab51e
adoleba <ania.doleba@wp.pl>
2020-10-14 21:25:47 +0200

    create category - validate unique name

--- blog_pyramid/forms/category.py
+++ blog_pyramid/forms/category.py
@@ -1,7 +1,15 @@
 import colander
 import deform.widget
 
+from blog_pyramid.models.category import Category
+
+
+def validate_unique_name(node, appstruct, dbsession):
+    if dbsession.query(Category).filter_by(name=appstruct['name']).first():
+        raise colander.Invalid(node, "Category already taken")
+
 
 class CategoryForm(colander.MappingSchema):
-    name = colander.SchemaNode(colander.String(), validator=colander.Length(max=20))
+    name = colander.SchemaNode(colander.String())
     description = colander.SchemaNode(colander.String(), validator=colander.Length(max=100), widget=deform.widget.TextAreaWidget())
+
--- blog_pyramid/views/admin.py
+++ blog_pyramid/views/admin.py
@@ -1,12 +1,16 @@
 import datetime
+from functools import partial
+
+import colander
 import transaction
 
 import deform
-from deform import Form
+from deform import Form, ValidationFailure
 from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
+from slugify import slugify
 
-from blog_pyramid.forms.category import CategoryForm
+from blog_pyramid.forms.category import CategoryForm, validate_unique_name
 from blog_pyramid.forms.post import PostForm
 from blog_pyramid.models.category import Category
 from blog_pyramid.services.categories import CategoryService
@@ -58,10 +62,10 @@ class CategoriesViews:
 
     @property
     def category_form(self):
-        category_form = CategoryForm()
+        category_form = CategoryForm(validator=partial(validate_unique_name, dbsession=self.request.dbsession)).bind(request=self.request) # validator = partial(validate_unique_name, dbsession)
         submit = deform.Button(name='Save', css_class='btn btn-info')
         cancel = deform.Button(name='Cancel', css_class='btn btn-inverse')
-        return Form(category_form, buttons=(submit, cancel))
+        return Form(category_form, buttons=(submit, cancel, ))
 
     @view_config(route_name='admin_categories', renderer='../templates/admin/categories/categories_list.jinja2')
     def admin_categories(self):
@@ -73,22 +77,29 @@ class CategoriesViews:
     def category_create(self):
         title = 'Create a category'
 
-        form = self.category_form.render()
+        form = self.category_form
 
         if 'Save' in self.request.params:
-            category_name = self.request.params.get('name')
-            category_description = self.request.params.get('description')
+            name = self.request.params.get('name')
+            description = self.request.params.get('description')
+            controls = self.request.POST.items()
 
-            new_category = Category(name=category_name, description=category_description)
-            self.request.dbsession.add(new_category)
+            try:
+                form.validate(controls)
+                new_category = Category(name=name, description=description)
 
-            url = self.request.route_url('admin_categories')
-            return HTTPFound(location=url)
+                self.request.dbsession.add(new_category)
+
+                url = self.request.route_url('admin_categories')
+                return HTTPFound(location=url)
+
+            except ValidationFailure as e:
+                return {'form': e.render()}
 
         if 'Cancel' in self.request.params:
             return HTTPFound(location=self.request.route_url('admin_categories'))
 
-        return {'title': title, 'form': form}
+        return {'title': title, 'form': form.render()}
 
     @view_config(route_name='category_edit', renderer='../templates/admin/categories/category_create_edit.jinja2')
     def category_edit(self):
@@ -103,8 +114,9 @@ class CategoriesViews:
         if 'Save' in self.request.params:
             new_category_name = self.request.params.get('name')
             new_category_description = self.request.params.get('description')
+            new_category_slug = slugify(new_category_name)
             self.request.dbsession.query(Category).filter(Category.slug == slug)\
-                .update({'name': new_category_name, 'description': new_category_description})
+                .update({'slug': new_category_slug, 'description': new_category_description, 'name': new_category_name})
 
             url = self.request.route_url('admin_categories')
             return HTTPFound(location=url)
