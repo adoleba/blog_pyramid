a870287cf12dfa137b5d9c82b197e78556690851
adoleba <ania.doleba@wp.pl>
2020-11-03 15:39:22 +0100

    blog page, init post_page

--- blog_pyramid/routes.py
+++ blog_pyramid/routes.py
@@ -1,7 +1,10 @@
 def includeme(config):
     config.add_static_view('static', 'static', cache_max_age=3600)
-    # main
+    #blog
     config.add_route('index', '/')
+    config.add_route('post_page', '/{slug}')
+
+    # admin - main
     config.add_route('admin', '/admin')
     config.add_route('login', '/admin/login')
     config.add_route('logout', '/admin/logout')
--- blog_pyramid/views/blog.py
+++ blog_pyramid/views/blog.py
@@ -1,7 +1,34 @@
 from pyramid.view import view_config
 
+from blog_pyramid.models import Post
+from blog_pyramid.services.categories import CategoryService
+from blog_pyramid.services.posts import PostService
 
-@view_config(route_name='index', renderer='../templates/blog/posts.jinja2', permission='view')
-def index(request):
-    title = 'Welcome to my blog'
-    return {'title': title}
+
+class PostsViews:
+    def __init__(self, request):
+        self.request = request
+
+    @property
+    def slug(self):
+        slug = self.request.matchdict['slug']
+        return slug
+
+    @property
+    def post(self):
+        post = self.request.dbsession.query(Post).filter_by(slug=self.slug).one()
+        return post
+
+    @property
+    def categories(self):
+        categories = CategoryService.all(self.request)
+        return categories
+
+    @view_config(route_name='index', renderer='../templates/blog/posts.jinja2', permission='view')
+    def index(self):
+        posts = PostService.all(self.request)
+        return {'posts': posts, 'categories': self.categories}
+
+    @view_config(route_name='post_page', renderer='../templates/blog/post_page.jinja2', permission='view')
+    def post_page(self):
+        return {'post': self.post,  'categories': self.categories}
