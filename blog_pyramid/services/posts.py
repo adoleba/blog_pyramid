fa66e2d0bebbc32be2729a82a54b58495bd9d554
adoleba <ania.doleba@wp.pl>
2020-11-24 15:39:05 +0100

    pagination for blog

--- blog_pyramid/services/posts.py
+++ blog_pyramid/services/posts.py
@@ -38,13 +38,34 @@ class PostService:
 class PostServiceBlog:
 
     @classmethod
-    def all(cls, request):
-        return request.dbsession.query(Post).order_by(Post.created.desc())
+    def all(cls, request, page=1):
+        query = request.dbsession.query(Post).order_by(Post.created.desc())
+        query_params = request.GET.mixed()
+
+        def url_maker(link_page):
+            query_params['page'] = link_page
+            return request.current_route_url(_query=query_params)
+
+        return paginate_sqlalchemy.SqlalchemyOrmPage(query, page, items_per_page=3, url_maker=url_maker)
 
     @classmethod
-    def by_category(cls, request, category):
-        return request.dbsession.query(Post).filter(Post.category == category).order_by(Post.created.desc())
+    def by_category(cls, request, category, page=1):
+        query = request.dbsession.query(Post).filter(Post.category == category).order_by(Post.created.desc())
+        query_params = request.GET.mixed()
+
+        def url_maker(link_page):
+            query_params['page'] = link_page
+            return request.current_route_url(_query=query_params)
+
+        return paginate_sqlalchemy.SqlalchemyOrmPage(query, page, items_per_page=3, url_maker=url_maker)
 
     @classmethod
-    def by_user(cls, request, username):
-        return request.dbsession.query(Post).filter(Post.author == username).order_by(Post.created.desc())
+    def by_user(cls, request, username, page=1):
+        query = request.dbsession.query(Post).filter(Post.author == username).order_by(Post.created.desc())
+        query_params = request.GET.mixed()
+
+        def url_maker(link_page):
+            query_params['page'] = link_page
+            return request.current_route_url(_query=query_params)
+
+        return paginate_sqlalchemy.SqlalchemyOrmPage(query, page, items_per_page=3, url_maker=url_maker)
--- blog_pyramid/views/blog.py
+++ blog_pyramid/views/blog.py
@@ -24,10 +24,15 @@ class PostsViews:
         categories = CategoryService.all(self.request)
         return categories
 
+    @property
+    def page(self):
+        page = int(self.request.params.get('page', 1))
+        return page
+
     @view_config(route_name='index', renderer='../templates/blog/posts.jinja2', permission='view')
     def index(self):
         title = "Welcome to our blog"
-        posts = PostServiceBlog.all(self.request)
+        posts = PostServiceBlog.all(request=self.request, page=self.page)
         return {'posts': posts, 'categories': self.categories, 'title': title}
 
     @view_config(route_name='blog_category_posts', renderer='../templates/blog/posts.jinja2', permission='view')
@@ -35,14 +40,14 @@ class PostsViews:
         category_slug = self.request.matchdict['category_slug']
         category = self.request.dbsession.query(Category).filter_by(slug=category_slug).one()
         category_name = category.name
-        posts = PostServiceBlog.by_category(self.request, category_name)
+        posts = PostServiceBlog.by_category(request=self.request, page=self.page, category=category_name)
         title = "Posts from category " + category_name
         return {'title': title, 'categories': self.categories, 'posts': posts}
 
     @view_config(route_name='blog_user_posts', renderer='../templates/blog/posts.jinja2', permission='view')
     def blog_user_posts(self):
         username = self.request.matchdict['username']
-        posts = PostServiceBlog.by_user(self.request, username)
+        posts = PostServiceBlog.by_user(self.request, page=self.page, username=username)
         title = "Posts from user " + username
         return {'title': title, 'categories': self.categories, 'posts': posts}
 
